55f675e134b3e019d6abffcb1f0a0db7027e5b64
craighaber <craighaber1@gmail.com>
2023-11-26 15:01:13 -0500

    Fleshed out sample server

---
+++ server/server.js
@@ -0,0 +1,45 @@
+import express from 'express'
+import cors from 'cors'
+import dotenv from 'dotenv'
+import {getNote, getNotes, createNote} from './database.js'
+
+dotenv.config()
+
+
+
+const app = express()
+app.use(cors())
+//Allows express to parse JSON request bodies
+app.use(express.json()) 
+
+app.get('/', (req, res)=>{
+    return res.json("root")
+})
+
+app.get('/notes', async (req, res)=> {   
+    const notes = await getNotes()
+    res.send(notes) 
+})
+
+app.get('/notes/:id', async (req, res)=> {   
+    const id = req.params.id
+    const note = await getNote(id)
+    res.send(note) 
+})
+
+app.post('/notes', async (req, res)=> {   
+    const {title, contents} = req.body
+    const id = await createNote(title, contents) 
+    res.status(201).send(id)
+})
+
+// Global error handler
+app.use((err, req, res, next) => {
+    console.error(err.stack)
+    res.status(500).send('Unexpected Server Error')
+})
+
+app.listen(process.env.NODE_PORT), () =>{
+    console.log("listening")
+}
+
