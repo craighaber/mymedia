266ffb9a5754496ad2b6882aaffe93910e391a51
craighaber <craighaber1@gmail.com>
2024-01-10 18:01:01 -0500

    Got google login working

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,22 +1,22 @@
-import { useState } from 'react'
 import './styles.scss'
 import Navbar from './features/navbar/components/Navbar/Navbar'
 import Home from './features/home/components/Home/Home'
 import { Route, Routes } from 'react-router-dom'
 import Login from './features/login/components/Login/Login'
 import Account from './features/account/components/Account/Account'
+import { AuthContextProvider } from './AuthContext'
 
 function App() {
-  const [count, setCount] = useState(0)
-
   return (
     <>
-      <Navbar/>
-      <Routes>
-        <Route path='/' element={<Home/>} />
-        <Route path='/login' element={<Login/>} />
-        <Route path='/account' element={<Account/>} />
-      </Routes>
+      <AuthContextProvider>
+        <Navbar/>
+        <Routes>
+          <Route path='/' element={<Home/>} />
+          <Route path='/login' element={<Login/>} />
+          <Route path='/account' element={<Account/>} />
+        </Routes>
+      </AuthContextProvider>
     </>
   )
 }
--- client/src/features/login/components/Login/Login.tsx
+++ client/src/features/login/components/Login/Login.tsx
@@ -1,11 +1,22 @@
 import GoogleButton from 'react-google-button'
 import './Login.scss'
+import { UserAuth } from '../../../../AuthContext'
 
 function Login(){
+
+    const {googleLogIn}: any = UserAuth()
+
+    const handeGoogleLogin = async () => {
+        try {
+            await googleLogIn()
+        } catch (error){
+            console.log(error)
+        }
+    }
     return( 
     <> 
         <p>Login page</p>
-        <GoogleButton />
+        <GoogleButton onClick={handeGoogleLogin} />
     </>)
 }
 
