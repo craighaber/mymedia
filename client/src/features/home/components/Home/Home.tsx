e80996f7cf8f86e6e5bef998c76ba3e32eb50ce1
craighaber <craighaber1@gmail.com>
2024-01-11 20:01:52 -0500

    Added checkmarks

--- client/src/features/home/components/Home/Home.tsx
+++ client/src/features/home/components/Home/Home.tsx
@@ -1,6 +1,8 @@
 import { useNavigate } from 'react-router-dom';
 import './Home.scss';
 import RoutePaths from '../../../../globals/constants/RoutePaths';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faCheck } from '@fortawesome/free-solid-svg-icons'
 
 export default function Home(){
     const navigate = useNavigate()
@@ -21,9 +23,9 @@ export default function Home(){
 
         <div className='subtitles'>
             <ul>
-                <li>With ratings and reviews</li>
-                <li>All in one place</li>
-                <li>For free</li>
+                <li><FontAwesomeIcon icon={faCheck}/> With ratings and reviews</li>
+                <li><FontAwesomeIcon icon={faCheck}/> All in one place</li>
+                <li><FontAwesomeIcon icon={faCheck}/> For free</li>
             </ul>
  
         </div>
