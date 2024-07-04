94a226a63a0e544d4ce17cf5a4906311ae48e087
Craig Haber <craighaber1@gmail.com>
2024-07-04 18:04:40 -0400

    Shifting db to postgres

--- TODO.md
+++ TODO.md
@@ -1,10 +1,11 @@
 ## TODO ##
-- Animation for mobile menu dropping down (should be easy)
-- Carosuel with images of different kinds of media on landing page?
 - Host the site and db baby
-
+-   Best idea is to use as DigitalOcean droplet for 4$ a month
+-   Probably some promo code I could use for a few months for free
+-   Can follow this tutorial for client and server: https://www.youtube.com/watch?v=2kvA6Ba3fWo&list=FLWGhxFgbmN5jj3iFQf8hYgg
+-   Firebase should just be matter of setting up env variables in a file
+-   MySql: https://www.youtube.com/watch?v=WltqUaqxBH8
 ## Low priority ##
 - Show error if title blank for edit entry
 -  edit entry not always centered vertically
-- export to csv
 - Forgot Password
--- server/database.js
+++ server/database.js
@@ -1,47 +1,48 @@
-import mysql from 'mysql2'
+import pgPromise from 'pg-promise'
 import dotenv from 'dotenv'
 dotenv.config()
 
 
-const pool = mysql.createPool({
-    host: process.env.MYSQL_HOST,
-    user: process.env.MYSQL_USER,
-    password: process.env.MYSQL_PASSWORD,
-    database: process.env.MYSQL_DATABASE
-}).promise()
 
+const pgp = pgPromise();
+const connection = {
+    host: process.env.PG_HOST,
+    port: process.env.PG_PORT,
+    database: process.env.PG_DATABASE,
+    user: process.env.PG_USER,
+    password: process.env.PG_PASSWORD,
+    max: 30 // Max number of simaltaneous connections
+}
+
+const db = pgp(connection)
 
 export async function getMedia(userId){
-    const [rows] = await pool.query('SELECT media.id, media.title, categories.category, media.rating, media.review, media.notes from media JOIN categories ON media.category_id = categories.id WHERE media.user_id = ? ORDER BY  media.title', [userId])
+    const rows = await db.manyOrNone('SELECT media.id, media.title, categories.category, media.rating, media.review, media.notes from media JOIN categories ON media.category_id = categories.id WHERE media.user_id = $1 ORDER BY  media.title', [userId])
     return rows
 }
 
 export async function getMediaEntry(id){
-    const [rows] = await pool.query('SELECT media.id, media.title, categories.category, media.rating, media.review, media.notes from media JOIN categories ON media.category_id = categories.id WHERE media.id = ?', [id])
-    if (rows.length === 0){
-        return null
-    } else {
-        return rows[0]
-    }
+    const row = await db.oneOrNone('SELECT media.id, media.title, categories.category, media.rating, media.review, media.notes from media JOIN categories ON media.category_id = categories.id WHERE media.id = $1', [id])
+    return row
 }
 
 export async function addMediaEntry(title, category, rating, review, userId){
-    await pool.query(
-    `INSERT INTO media (title, category_id, rating, review, user_id) VALUES (?, (SELECT id FROM categories WHERE category = ?), ?, ?, ?)`, 
+    await db.none(
+    `INSERT INTO media (title, category_id, rating, review, user_id) VALUES ($1, (SELECT id FROM categories WHERE category = $2), $3, $4, $5)`, 
     [title, category, rating, review, userId])
     
 }
 
 export async function updateMediaEntry(id, title, category, rating, review, notes){
-    await pool.query('UPDATE media SET title=?, category_id=(SELECT id from categories WHERE category = ?), rating=?, review=?, notes=? WHERE id = ?',
+    await db.none('UPDATE media SET title=$1, category_id=(SELECT id from categories WHERE category = $2), rating=$3, review=$4, notes=$5 WHERE id = $6',
     [title, category, rating, review, notes, id])
 }
 
 export async function deleteMediaEntry(id){
-    await pool.query('DELETE FROM media WHERE id = ?', [id])
+    await db.none('DELETE FROM media WHERE id = $1', [id])
 }
 
 export async function getCategories(){
-    const [rows] = await pool.query("SELECT category from categories")
+    const rows =  await db.manyOrNone("SELECT category from categories")
     return rows.map(row => row.category)
 }
