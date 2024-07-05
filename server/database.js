7c1dbc62ae9e84b34da31b6fa9ea7bb3dd73e0ec
Craig Haber <craighaber1@gmail.com>
2024-07-04 20:07:02 -0400

    Changing env variables

--- server/database.js
+++ server/database.js
@@ -6,11 +6,11 @@ dotenv.config()
 
 const pgp = pgPromise();
 const connection = {
-    host: process.env.PG_HOST,
-    port: process.env.PG_PORT,
-    database: process.env.PG_DATABASE,
-    user: process.env.PG_USER,
-    password: process.env.PG_PASSWORD,
+    host: process.env.POSTGRES_HOST,
+    port: process.env.POSTGRES_PORT,
+    database: process.env.POSTGRES_DATABASE,
+    user: process.env.POSTGRES_USER,
+    password: process.env.POSTGRES_PASSWORD,
     max: 30 // Max number of simaltaneous connections
 }
 
