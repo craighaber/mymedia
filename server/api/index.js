6091c76596b742467b286445ab142871a6ad4978
Craig Haber <craighaber1@gmail.com>
2024-07-20 19:28:06 -0400

    Broadening CORS policy

--- server/api/index.js
+++ server/api/index.js
@@ -8,7 +8,7 @@ dotenv.config()
 const app = express()
 app.use(cors(
     {
-        origin: process.env.CLIENT_URL,
+        origin: '*',
         credentials: true,
         methods: ['GET', 'POST', 'PUT', 'DELETE']
     }
