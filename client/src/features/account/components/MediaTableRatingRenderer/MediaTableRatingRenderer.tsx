a47e85708a09c29c9e46760c2430aa03b3f90f62
Craig Haber <craighaber1@gmail.com>
2024-09-28 22:39:29 -0400

    Hopefully fixing 10-scale problem

--- client/src/features/account/components/MediaTableRatingRenderer/MediaTableRatingRenderer.tsx
+++ client/src/features/account/components/MediaTableRatingRenderer/MediaTableRatingRenderer.tsx
@@ -6,8 +6,11 @@ export default function MediaTableRatingRenderer(params: any){
 
     const ratingSize = isSmallScreen ? 'medium' : 'large'; 
 
-    const rating = params?.data?.rating;
+    let rating = params?.data?.rating;
+    // Making rating backwards compatible with 10-scale
+    if (rating && rating > 5) rating = rating / 2;
  
+
     return (
         <div className="rating-container">
             {rating && <Rating name="rating" className="star-rating--readonly" precision={0.25} size={ratingSize} value={rating} readOnly /> }  
