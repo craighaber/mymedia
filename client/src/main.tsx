c3dc1540945d60a379f27645504798ea4e00fc6c
Craig Haber <craighaber1@gmail.com>
2026-07-26 22:36:10 -0400

    Revert "Adding analytics"
    This reverts commit b21e58f84d79c97c013443b53b001cb1c8a1c505.

--- client/src/main.tsx
+++ client/src/main.tsx
@@ -2,11 +2,9 @@ import React from 'react'
 import ReactDOM from 'react-dom/client'
 import App from './App'
 import { BrowserRouter } from 'react-router-dom'
-import { Analytics } from '@vercel/analytics/next';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <BrowserRouter>
-    <Analytics />
     <App />
   </BrowserRouter>,
 )
